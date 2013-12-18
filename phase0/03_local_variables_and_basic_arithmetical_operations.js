c45555803f0b6ab4b2c3722865dfb64baab22133
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-12-17 22:31:58 -0800

    basic arithmetic

--- phase0/03_local_variables_and_basic_arithmetical_operations.js
+++ phase0/03_local_variables_and_basic_arithmetical_operations.js
@@ -4,3 +4,48 @@
 
 // For the quotient, be sure to convert the variables to Floats before dividing.
 
+var chai   = require('chai')
+  , expect = chai.expect
+
+var num1 = 3
+  , num2 = 2
+  , sum  = num1 + num2
+  , difference = num1 - num2
+  , quotient = num1 / num2
+  , product = num1 * num2
+  , modulus = num1 % num2
+
+describe('sum', function() {
+  it('returns the sum of num1 and num2', function(done) {
+    expect(sum).to.equal(5)
+    done()
+  })
+})
+
+describe('difference', function() {
+  it('returns the difference of num1 and num2', function(done) {
+    expect(difference).to.equal(1)
+    done()
+  })
+})
+
+describe('quotient', function() {
+  it('returns the quotient of num1 and num2', function(done) {
+    expect(quotient).to.equal(1.5)
+    done()
+  })
+})
+
+describe('product', function() {
+  it('returns the product of num1 and num2', function(done) {
+    expect(product).to.equal(6)
+    done()
+  })
+})
+
+describe('modulus', function() {
+  it('returns the modulus of num1 and num2', function(done) {
+    expect(modulus).to.equal(1)
+    done()
+  })
+})
