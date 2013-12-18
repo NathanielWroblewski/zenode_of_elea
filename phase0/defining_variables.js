42e82bd346a4f2ee7bbc45ab5ae7afbd3b4e0299
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-12-17 21:55:47 -0800

    initial commit

---
+++ phase0/defining_variables.js
@@ -0,0 +1,30 @@
+var chai   = require('chai')
+  , expect = chai.expect
+
+var firstName = 'Nathaniel'
+  , lastName  = 'Wroblewski'
+  , age       = 28
+
+describe('firstName', function() {
+  it ("returns the value 'Nathaniel'", function(done) {
+      expect(firstName).to.be.a('string')
+      expect(firstName).to.equal('Nathaniel')
+    done()
+  })
+})
+
+describe('lastName', function() {
+  it ("returns the value 'Wroblewski'", function(done) {
+      expect(lastName).to.be.a('string')
+      expect(lastName).to.equal('Wroblewski')
+    done()
+  })
+})
+
+describe('age', function() {
+  it ("returns the value 'Wroblewski'", function(done) {
+      expect(age).to.be.a('number')
+      expect(age).to.equal(28)
+    done()
+  })
+})
