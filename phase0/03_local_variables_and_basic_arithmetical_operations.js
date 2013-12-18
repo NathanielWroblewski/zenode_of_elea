ddf05e158c17497a99686856f1305ffb00e7f112
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-12-17 22:21:09 -0800

    rename to preserve order

---
+++ phase0/03_local_variables_and_basic_arithmetical_operations.js
@@ -0,0 +1,6 @@
+// Create two integer variables called num1 and num2 and assign them your favorite numbers.
+
+// Next, compute the sum, difference, quotient, product, and modulus of these two numbers and assign these values to variables called sum, difference, quotient, product, and modulus, respectively.
+
+// For the quotient, be sure to convert the variables to Floats before dividing.
+
